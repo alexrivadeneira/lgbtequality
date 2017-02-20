681646550808d55483fb50330b24c11f44251229
Alex Rivadeneira <alexrivs@gmail.com>
2017-02-19 20:59:48 -0800

    test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,160 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Pledge.create(firstname: "Alex", 
+	lastname: "Riva", 
+	email: "alex@berkeley.edu", 
+	program: "Full time MBA", 
+	gradyear: 2017, 
+	cohort: "Blue",
+	public: true,
+	additional: "",
+	reason: "",
+	assist: "No, I'm busy now, but keep me informed!",
+	intl: false,
+	intlcity: ""
+	)
+
+Pledge.create(firstname: "Dan", 
+	lastname: "Riva", 
+	email: "alex3@berkeley.edu", 
+	program: "Full time MBA", 
+	gradyear: 2017, 
+	cohort: "Blue",
+	public: true,
+	additional: "",
+	reason: "",
+	assist: "No, I'm busy now, but keep me informed!",
+	intl: false,
+	intlcity: ""
+	)
+
+Pledge.create(firstname: "Dan", 
+	lastname: "Rather", 
+	email: "alex2@berkeley.edu", 
+	program: "Full time MBA", 
+	gradyear: 2017, 
+	cohort: "Blue",
+	public: true,
+	additional: "",
+	reason: "",
+	assist: "No, I'm busy now, but keep me informed!",
+	intl: false,
+	intlcity: ""
+	)
+
+Pledge.create(firstname: "Theodore", 
+	lastname: "Roosevelt", 
+	email: "alex3@berkeley.edu", 
+	program: "Full time MBA", 
+	gradyear: 2017, 
+	cohort: "Blue",
+	public: true,
+	additional: "",
+	reason: "",
+	assist: "No, I'm busy now, but keep me informed!",
+	intl: false,
+	intlcity: ""
+	)
+
+Pledge.create(firstname: "Shane", 
+	lastname: "Livingston", 
+	email: "alex3@tuck.edu", 
+	program: "Full time MBA", 
+	gradyear: 2017, 
+	cohort: "Blue",
+	public: true,
+	additional: "",
+	reason: "",
+	assist: "No, I'm busy now, but keep me informed!",
+	intl: false,
+	intlcity: ""
+	)
+
+Pledge.create(firstname: "Gordon", 
+	lastname: "Brown", 
+	email: "gbrown@tuck.edu", 
+	program: "Full time MBA", 
+	gradyear: 2017, 
+	cohort: "Blue",
+	public: true,
+	additional: "",
+	reason: "",
+	assist: "No, I'm busy now, but keep me informed!",
+	intl: false,
+	intlcity: ""
+	)
+
+Pledge.create(firstname: "Matt", 
+	lastname: "Law", 
+	email: "gbrown@michigan.edu", 
+	program: "Full time MBA", 
+	gradyear: 2017, 
+	cohort: "Blue",
+	public: true,
+	additional: "",
+	reason: "",
+	assist: "No, I'm busy now, but keep me informed!",
+	intl: false,
+	intlcity: ""
+	)
+
+Pledge.create(firstname: "Chris", 
+	lastname: "Law", 
+	email: "claw@michigan.edu", 
+	program: "Full time MBA", 
+	gradyear: 2017, 
+	cohort: "Blue",
+	public: true,
+	additional: "",
+	reason: "",
+	assist: "No, I'm busy now, but keep me informed!",
+	intl: false,
+	intlcity: ""
+	)
+
+Pledge.create(firstname: "Ebenezer", 
+	lastname: "Scrooge", 
+	email: "claw@stanford.edu", 
+	program: "Full time MBA", 
+	gradyear: 2017, 
+	cohort: "Blue",
+	public: true,
+	additional: "",
+	reason: "",
+	assist: "No, I'm busy now, but keep me informed!",
+	intl: false,
+	intlcity: ""
+	)
+
+Pledge.create(firstname: "Lincoln", 
+	lastname: "Scrooge", 
+	email: "clawe@stanford.edu", 
+	program: "Full time MBA", 
+	gradyear: 2017, 
+	cohort: "Blue",
+	public: true,
+	additional: "",
+	reason: "",
+	assist: "No, I'm busy now, but keep me informed!",
+	intl: false,
+	intlcity: ""
+	)
+
+  # create_table "pledges", force: :cascade do |t|
+  #   t.string   "firstname"
+  #   t.string   "lastname"
+  #   t.string   "email"
+  #   t.string   "program"
+  #   t.integer  "gradyear"
+  #   t.string   "cohort"
+  #   t.boolean  "public"
+  #   t.string   "additional"
+  #   t.string   "reason"
+  #   t.string   "assist"
+  #   t.boolean  "intl"
+  #   t.string   "intlcity"
+  #   t.datetime "created_at", null: false
+  #   t.datetime "updated_at", null: false
+  # end
