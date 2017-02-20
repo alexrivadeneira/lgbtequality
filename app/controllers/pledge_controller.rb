ecaa0acc0ab234d547ed333165ea2185035f9eae
Alex Rivadeneira <alexrivs@gmail.com>
2017-02-19 20:46:22 -0800

    add initial chart

--- app/controllers/pledge_controller.rb
+++ app/controllers/pledge_controller.rb
@@ -1,4 +1,5 @@
 class PledgeController < ApplicationController
+  
   def form
   	@pledge = Pledge.new
   end
@@ -22,13 +23,40 @@ class PledgeController < ApplicationController
   def who
     @pledges = Pledge.all
 
+
     @publicpledges = []
+    @public_emails = []
+
     @pledges.each do |pledge|
       if pledge.public
-        @publicpledges.push(pledge)
+        @publicpledges.push(pledge.firstname)
+        @public_emails.push(pledge.email.downcase.split("@")[1].split(".edu")[0].capitalize)
       end
     end
-   
+
+    @school_counts = Hash.new
+
+    @public_emails.each do |school|
+      if @school_counts[school]
+        @school_counts[school] += 1
+      else
+        @school_counts[school] = 1
+      end
+    end
+
+    max_school_count = 0
+
+    @school_counts.each do |school, count|
+      if count > max_school_count
+        max_school_count = count
+      end
+    end
+
+    @multiplier = 100 / max_school_count
+
+
   end
+
+
 
 end
