09128c56def610d4470f094d86a1b9629ce5ca19
alexrivadeneira <alexrivs@gmail.com>
2017-04-07 10:46:31 -0700

    design

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,13 @@
 //= require turbolinks
 //= require_tree .
 //= require bootstrap.min
+
+function scrollToTop(scrollDuration) {
+    var scrollStep = -window.scrollY / (scrollDuration / 15),
+        scrollInterval = setInterval(function(){
+        if ( window.scrollY != 0 ) {
+            window.scrollBy( 0, scrollStep );
+        }
+        else clearInterval(scrollInterval); 
+    },15);
+}
