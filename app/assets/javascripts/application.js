cad7a65870bb3989af63415c70dabb49266f8e21
alexrivadeneira <alexrivs@gmail.com>
2017-04-07 11:07:23 -0700

    ok

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,13 +15,3 @@
 //= require turbolinks
 //= require_tree .
 //= require bootstrap.min
-
-function scrollToTop(scrollDuration) {
-    var scrollStep = -window.scrollY / (scrollDuration / 15),
-        scrollInterval = setInterval(function(){
-        if ( window.scrollY != 0 ) {
-            window.scrollBy( 0, scrollStep );
-        }
-        else clearInterval(scrollInterval); 
-    },15);
-}
